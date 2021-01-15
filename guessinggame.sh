1cf8adce11932b1afbb2de700dc73b160f5bcf85
LWeatherbie <lwxb@shaw.ca>
2021-01-15 07:22:47 -0700

    Added comments to script

--- guessinggame.sh
+++ guessinggame.sh
@@ -6,8 +6,16 @@
 # and exits when the correct number os guessed.
 
 function rank_guess {
+# Function that takes the user's guess and returns a value based on that.
+# It determines the number of files in the directory and returns:
+#	0 - User's guess matches the number of files
+#	1 - User's guess is too high
+#	-1 - User's guess is too low
+
+	# get the number of files and set "result"
 	local file_count=$(ls | wc -l)
 
+	# compare with the user's guess
 	if [[ $1 -eq $file_count ]]
 	then
 		result=0
@@ -19,19 +27,24 @@ function rank_guess {
 	fi
 }
 
-
+# Start by setting a boolean to control the loop
 incorrect=true
 
+" Loop until a correct answer is provided
 while $incorrect
 do
+	# Print a blank line for readability then ask the user  for a guess
 	echo
 	echo "Guess how many files are in the current directory:"
 	read file_count
+	# get the  function to determine the "quality" of the guess
 	rank_guess $file_count
 
+	# Display the result
 	if [[ $result -eq 0 ]]
 	then
 		echo "That's Right!"
+		# In this case, set the flag so the while loop exits
 		incorrect=false
 	elif [[ $result -eq -1 ]]
 	then
