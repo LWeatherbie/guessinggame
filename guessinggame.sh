eb1898a28f5f2f3c1119eb96d5110dbfc30287ce
LWeatherbie <lwxb@shaw.ca>
2021-01-14 18:46:24 -0700

    Initialized git repository

---
+++ guessinggame.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# File: guessinggame.sh
+# Part of a Unix course offered by Courera.org
+# Prompts the user to guess the number of files in a directory.
+# Provides hints as to whether the guess is too high or too low
+# and exits when the correct number os guessed.
+
+
+
+echo "Guess how many files are in the current directory:"
+read file_count
+echo "You guessed $file_count" 
+
